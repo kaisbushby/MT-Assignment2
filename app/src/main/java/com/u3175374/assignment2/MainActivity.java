ad59bde6c72624adccce9412abec8676498928cf
Kai Bushby <kaisbushby@gmail.com>
2019-05-02 15:02:45 +1000

    Added Map and UC logo into the files

--- app/src/main/java/com/u3175374/assignment2/MainActivity.java
+++ app/src/main/java/com/u3175374/assignment2/MainActivity.java
@@ -9,7 +9,17 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
     }
 
     @Override
@@ -49,4 +64,44 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        LatLng UC = new LatLng(-35.2384551,149.0844455);
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UC, 13));
+
+        // Flat markers will rotate when the map is rotated,
+        // and change perspective when the map is tilted.
+        googleMap.addMarker(new MarkerOptions()
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_uc))
+                .position(UC)
+                .flat(true)
+                .rotation(245));
+
+        CameraPosition cameraPosition = CameraPosition.builder()
+                .target(UC)
+                .zoom(15)
+                .bearing(90)
+                .build();
+
+        // Animate the change in camera view over 2 seconds
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition),
+                2000, null);
+
+        googleMap.addPolyline(new PolylineOptions().geodesic(true)
+                .add(new LatLng(-35.230926, 149.080354))
+                .add(new LatLng(-35.234634, 149.091888))
+                .add(new LatLng(-35.242096, 149.090257))
+                .add(new LatLng(-35.2429721, 149.0736905))
+                .add(new LatLng(-35.230926, 149.080354))
+        );
+
+
+    }
+
 }
