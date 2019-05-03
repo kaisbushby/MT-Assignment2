bb3541db879c10cc8ff3c98d04c71acacb86297f
Kai Bushby <kaisbushby@gmail.com>
2019-05-03 11:38:17 +1000

    Added Map and UC logo into the files

--- app/src/main/java/com/u3175374/assignment2/MainActivity.java
+++ app/src/main/java/com/u3175374/assignment2/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,10 +18,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+    GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +64,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.normal_render) {
+            googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            return true;
+        }
+        if (id == R.id.satellite_render) {
+            googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
             return true;
         }
 
@@ -67,22 +78,105 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        LatLng sydney = new LatLng(-33.867, 151.206);
-
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+        this.googleMap = googleMap;
 
         LatLng UC = new LatLng(-35.2384551,149.0844455);
+        LatLng CentralLocation = new LatLng(-35.238887, 149.084593);
+        LatLng Library = new LatLng(-35.23803, 149.083405);
+        LatLng UCCooperLodge = new LatLng(-35.238849, 149.082214);
+        LatLng StudentResourceCenter = new LatLng(-35.236428, 149.084288);
+        LatLng TheHub = new LatLng(-35.23844,149.08452);
+        LatLng UCGYM = new LatLng(-35.238318, 149.088285);
+        LatLng StreetViewTop = new LatLng(-35.233653, 149.087192);
+        LatLng StreetViewBottom = new LatLng(-35.238516, 149.089560);
 
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UC, 13));
 
         // Flat markers will rotate when the map is rotated,
         // and change perspective when the map is tilted.
-        googleMap.addMarker(new MarkerOptions()
+        //------------------------------------------------------------------------------------------
+        final Marker ucMarker = googleMap.addMarker(new MarkerOptions()
                 .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_uc))
                 .position(UC)
                 .flat(true)
-                .rotation(245));
+                .rotation(245)
+                .title("University of Canberra")
+                .snippet("Bruce Campus")
+        );
 
+        final Marker CentralLocationMarker = googleMap.addMarker(new MarkerOptions()
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_uc))
+                .position(CentralLocation)
+                .flat(true)
+                .rotation(245)
+                .title("Central Location")
+                .snippet("University of Canberra")
+        );
+
+        final Marker LibraryMarker = googleMap.addMarker(new MarkerOptions()
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_uc))
+                .position(Library)
+                .flat(true)
+                .rotation(245)
+                .title("Library")
+                .snippet("University of Canberra")
+        );
+
+        final Marker UCCooperLodgeMarker = googleMap.addMarker(new MarkerOptions()
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_uc))
+                .position(UCCooperLodge)
+                .flat(true)
+                .rotation(245)
+                .title("Student Resource Center")
+                .snippet("University of Canberra")
+        );
+
+        final Marker StudentResourceCenterMarker = googleMap.addMarker(new MarkerOptions()
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_uc))
+                .position(StudentResourceCenter)
+                .flat(true)
+                .rotation(245)
+                .title("Student Resource Center")
+                .snippet("University of Canberra")
+        );
+
+        final Marker TheHubMarker = googleMap.addMarker(new MarkerOptions()
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_uc))
+                .position(TheHub)
+                .flat(true)
+                .rotation(245)
+                .title("The Hub")
+                .snippet("University of Canberra")
+        );
+
+        final Marker UCGymMarker = googleMap.addMarker(new MarkerOptions()
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_uc))
+                .position(UCGYM)
+                .flat(true)
+                .rotation(245)
+                .title("UC GYM")
+                .snippet("University of Canberra")
+        );
+
+        final Marker StreetViewTopMarker = googleMap.addMarker(new MarkerOptions()
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_uc))
+                .position(StreetViewTop)
+                .flat(true)
+                .rotation(245)
+                .title("Street View Top")
+                .snippet("University of Canberra")
+        );
+
+        final Marker StreetViewBottomMarker = googleMap.addMarker(new MarkerOptions()
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_uc))
+                .position(StreetViewBottom)
+                .flat(true)
+                .rotation(245)
+                .title("Street View Bottom")
+                .snippet("University of Canberra")
+        );
+
+        //------------------------------------------------------------------------------------------
         CameraPosition cameraPosition = CameraPosition.builder()
                 .target(UC)
                 .zoom(15)
@@ -101,6 +195,40 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .add(new LatLng(-35.230926, 149.080354))
         );
 
+        Polygon polygon1 = googleMap.addPolygon(new PolygonOptions()
+                .clickable(true)
+                .fillColor(0xffF9A825)      // Hex code for blue
+                .add(
+                        new LatLng(-27.457, 153.040),
+                        new LatLng(-33.852, 151.211),
+                        new LatLng(-37.813, 144.962),
+                        new LatLng(-34.928, 138.599)));
+        //------------------------------------------------------------------------------------------
+
+        googleMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+            @Override
+            public View getInfoWindow(Marker marker) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+                View infoWindow = getLayoutInflater().inflate(R.layout.infowindow_with_image, null);
+                TextView title = (TextView) infoWindow.findViewById(R.id.textViewTitle);
+                TextView snippet = (TextView) infoWindow.findViewById(R.id.textViewSnippet);
+                ImageView image = (ImageView) infoWindow.findViewById(R.id.imageView);
+
+                if (marker.getId().equals(ucMarker.getId())) {
+                    title.setText(marker.getTitle());
+                    snippet.setText(marker.getSnippet());
+                    image.setImageDrawable(getResources()
+                            .getDrawable(R.mipmap.ic_uc, getTheme()));
+                }
+
+
+                return infoWindow;
+            }
+        });
 
     }
 
---
+++ app/src/main/java/com/u3175374/assignment2/WebViewActivity.java
@@ -0,0 +1,51 @@
+package com.u3175374.assignment2;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewActivity extends AppCompatActivity {
+    WebView wv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        wv = (WebView) findViewById(R.id.webView);
+        wv.setWebViewClient(new MyWebView());
+        wv.getSettings().setLoadsImagesAutomatically(true);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        wv.loadUrl("http://www.canberra.edu.au/");
+
+    }
+
+    private class MyWebView extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(request.getUrl().toString());
+            return true;
+        }
+    }
+
+
+}
